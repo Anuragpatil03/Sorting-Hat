1ae75a2f86818eff40973ed3cf280ed6ddee6094
Anurag Patil <75440436+Anuragpatil03@users.noreply.github.com>
2021-03-01 23:14:57 +0530

    project file

---
+++ Sorthat.py
@@ -0,0 +1,17 @@
+#Program For Sorting Hat (Harry Potter)
+a=input('Enter your Name :')
+f=a[0]
+b=['a','h','t','r','g','y']
+c=['c','i','k','l','m','w']
+d=['o','q','f','e','u','v']
+e=['b','d','j','n','p','s']
+if f in b:
+    print("Your House is Gryfindorr")
+if f in c:
+    print("Your House is Revenclaw")
+if f in d:
+    print("Your House is Haffelpuff")      
+if f in e:
+    print("Your House is Slytherin") 
+
+     
